718e4a7630ca32912751d201480af906f62edf7e
alex501212 <56118464+alex501212@users.noreply.github.com>
2022-10-22 20:57:24 +0100

    intial server setup

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`server is running on port ${PORT}`));
