9b2327be2549316a71e2724152d4ffe7191eb5da
alex501212 <56118464+alex501212@users.noreply.github.com>
2022-10-22 20:27:37 +0100

    create intial pages and test chakra ui

---
+++ frontend/src/views/App.jsx
@@ -0,0 +1,13 @@
+import Login from "./Login/login";
+import Home from "./Home/home";
+import Search from "./Search/search";
+
+function App() {
+  return (
+    <div>
+      <Login></Login>
+    </div>
+  );
+}
+
+export default App;
