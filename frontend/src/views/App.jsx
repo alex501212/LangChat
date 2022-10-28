93b0bef6867eed605a3dae95d89e3593d0c70423
alex501212 <56118464+alex501212@users.noreply.github.com>
2022-10-29 00:23:03 +0100

    new home page, links between pages, new navheader

--- frontend/src/views/App.jsx
+++ frontend/src/views/App.jsx
@@ -1,20 +1,22 @@
-import Login from "./Login/login";
-import Home from "./Home/home";
-import Search from "./Search/search";
-import Register from "./Register/register";
-import Dashboard from "./Dashboard/dashboard";
+import Login from "./Login/Login";
+import Home from "./Home/Home";
+import Search from "./Search/Search";
+import Register from "./Register/Register";
+import Dashboard from "./Dashboard/Dashboard";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import NavHeader from "../components/NavHeader";
 
 function App() {
   return (
     <BrowserRouter>
+      <NavHeader />
       <Routes>
-      <Route path="/home" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/search" element={<Search />} />
-        <Route path="*" element={<Navigate to='/home' replace />} />
+        <Route path="*" element={<Navigate to="/home" replace />} />
       </Routes>
     </BrowserRouter>
   );
